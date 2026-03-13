98394631144accd0b3709a361c72db2f44273bc0
Nicholas T. <ntoutges@gmail.com>
2026-03-13 13:07:57 -0600

    Improved example rules

--- example/programs/pg_falling_sand.ts
+++ example/programs/pg_falling_sand.ts
@@ -2,6 +2,8 @@ import { cell_t, rule_t } from "../../src/module.js";
 
 // Import required cell/rule types
 import "../../src/cells/color/color.js";
+import "../../src/cells/set/set.js";
+import "../../src/cells/not/not.js";
 import "../../src/cells/range/range.js";
 import "../../src/rules/spatial/spatial.js";
 import "../../src/rules/sequence/sequence.js";
@@ -24,6 +26,34 @@ const sand = {
     },
 } satisfies cell_t;
 
+const stone = {
+    type: "color",
+    r: 150,
+    g: 150,
+    b: 150,
+} satisfies cell_t;
+
+const water = {
+    type: "color",
+    r: 0x39,
+    g: 0x6b,
+    b: 0x9c,
+} satisfies cell_t;
+
+const fish = {
+    type: "color",
+    r: 255,
+    g: 200,
+    b: 150,
+} satisfies cell_t;
+
+const fish_tail = {
+    type: "color",
+    r: 150,
+    g: 255,
+    b: 150,
+} satisfies cell_t;
+
 const air = {
     type: "color",
     r: 0,
@@ -31,70 +61,217 @@ const air = {
     b: 0,
 } satisfies cell_t;
 
+const fallable = {
+    type: "set",
+    cells: [water, air],
+} satisfies cell_t;
+
 // Fall sand down
-const ruled = {
+const sruled = {
+    type: "spatial",
+    before: `@
+             F`,
+    after: `A
+            @`,
+    scope: {
+        "@": sand,
+        A: air,
+        F: fallable,
+    },
+} satisfies rule_t;
+
+// Fall sand to right
+const sruler = {
+    type: "spatial",
+    before: `@ A1
+             B A1`,
+    after: `A A1
+            B @`,
+    scope: {
+        "@": sand,
+        B: sand,
+        A: air,
+        A1: fallable,
+        A2: fallable,
+    },
+} satisfies rule_t;
+
+// Fall sand to left
+const srulel = {
+    type: "spatial",
+    before: `A1 @
+             A2 B`,
+    after: `A1 A
+            @ B`,
+    scope: {
+        "@": sand,
+        B: sand,
+        A: air,
+        A1: fallable,
+        A2: fallable,
+    },
+} satisfies rule_t;
+
+// Fall water
+const wruled = {
     type: "spatial",
     before: `@
              A`,
     after: `A
             @`,
     scope: {
-        "@": sand,
+        "@": water,
         A: air,
     },
 } satisfies rule_t;
 
-// Fall sand to right
-const ruler = {
-    type: "spatial",
-    before: `@ A
-             B A`,
-    after: `A A
-            B @`,
-    scope: {
-        "@": sand,
-        B: sand,
-        A: air,
-    },
-} satisfies rule_t;
-
-// Fall sand to left
-const rulel = {
+// Expand water
+const wrulel = {
     type: "spatial",
     before: `A @
-             A B`,
-    after: `A A
-            @ B`,
+             _ W`,
+    after: `@ @
+            _ W`,
     scope: {
-        "@": sand,
-        B: sand,
+        "@": water,
+        W: {
+            type: "not",
+            cell: air,
+        },
         A: air,
+    },
+} satisfies rule_t;
+
+const wruler = {
+    type: "spatial",
+    before: `@ A
+             W _`,
+    after: `@ @
+            W _`,
+    scope: {
+        "@": water,
+        W: {
+            type: "not",
+            cell: air,
+        },
+        A: air,
+    },
+} satisfies rule_t;
+
+const frulesl = {
+    type: "spatial",
+    before: `W @ N`,
+    after: `T @ N`,
+    scope: {
+        "@": fish,
+        T: fish_tail,
+        W: water,
+        N: {
+            type: "not",
+            cell: {
+                type: "set",
+                cells: [fish, fish_tail],
+            },
+        },
+    },
+} satisfies rule_t;
+const frulesr = {
+    type: "spatial",
+    before: `N @ W`,
+    after: `N @ T`,
+    scope: {
+        "@": fish,
+        T: fish_tail,
+        W: water,
+        N: {
+            type: "not",
+            cell: {
+                type: "set",
+                cells: [fish, fish_tail],
+            },
+        },
+    },
+} satisfies rule_t;
+
+const frulemr = {
+    type: "spatial",
+    before: `T @ W`,
+    after: `W T @`,
+    scope: {
+        "@": fish,
+        T: fish_tail,
+        W: water,
+    },
+} satisfies rule_t;
+
+const fruleml = {
+    type: "spatial",
+    before: `W @ T`,
+    after: `@ T W`,
+    scope: {
+        "@": fish,
+        T: fish_tail,
+        W: water,
+    },
+} satisfies rule_t;
+
+const frulefl = {
+    type: "spatial",
+    before: `T @`,
+    after: `@ T`,
+    scope: {
+        "@": fish,
+        T: fish_tail,
+    },
+} satisfies rule_t;
+const frulefr = {
+    type: "spatial",
+    before: `@ T`,
+    after: `T @`,
+    scope: {
+        "@": fish,
+        T: fish_tail,
     },
 } satisfies rule_t;
 
 export const rule = {
     type: "sequence",
     rules: [
-        ruled,
+        sruled,
         {
             type: "quantum",
             weighted: true,
             rules: [
                 {
-                    rule: rulel,
+                    rule: srulel,
                     weight: 1,
                 },
                 {
-                    rule: ruler,
+                    rule: sruler,
                     weight: 1,
                 },
             ],
         },
+        wruled,
+        wrulel,
+        wruler,
+
+        {
+            type: "quantum",
+            rules: [frulesl, frulesr],
+        },
+        fruleml,
+        frulemr,
+        frulefl,
+        frulefr,
     ],
 } satisfies rule_t;
 
 export const cells = {
     sand,
+    stone,
+    water,
+    fish,
     air,
 };
 
